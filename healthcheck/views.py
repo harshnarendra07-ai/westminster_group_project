f193830444628dabcadc3cb3c5b27edd28e08db0
harshnarendra07 <harshnarendra07@gmail.com>
2026-03-15 15:50:36 +0000

    Added Schedule page URL routing, dynamic form, and POST request logic

--- healthcheck/views.py
+++ healthcheck/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .forms import MeetingForm
 
 def schedule_view(request):
-    return render(request, 'healthcheck/base.html')
+    if request.method == "POST":
+        form = MeetingForm(request.POST) 
+        if form.is_valid():
+            form.save() 
+            return redirect('schedule') 
+    else:
+        form = MeetingForm()
+    
+    context = {'form': form}
+    return render(request, 'healthcheck/schedule.html', context)
