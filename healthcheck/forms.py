5d3a1dc96de5e92106c6ef66330a17971dfb40c8
harshnarendra07 <harshnarendra07@gmail.com>
2026-04-09 03:43:32 +0100

    Schedule Calender has been added

--- healthcheck/forms.py
+++ healthcheck/forms.py
@@ -1,7 +1,43 @@
 from django import forms
 from .models import Meeting
+from django.core.exceptions import ValidationError
+from django.utils import timezone
 
 class MeetingForm(forms.ModelForm):
+    
+    PLATFORM_CHOICES = [
+        ('Microsoft Teams', 'Microsoft Teams'),
+        ('Zoom', 'Zoom'),
+        ('Google Meet', 'Google Meet'),
+        ('Slack Huddle', 'Slack Huddle'),
+        ('In-Person', 'In-Person'),
+    ]
+
+   
+    platform = forms.ChoiceField(
+        choices=PLATFORM_CHOICES, 
+        widget=forms.Select(attrs={'class': 'form-select'})
+    )
+
     class Meta:
         model = Meeting
-        fields = '__all__'  
+        fields = ['title', 'date_time', 'message', 'platform', 'organiser', 'team'] 
+        
+        widgets = {
+            'date_time': forms.DateTimeInput(attrs={'type': 'datetime-local', 'class': 'form-control'}),
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'message': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+            'organiser': forms.Select(attrs={'class': 'form-select'}),
+            'team': forms.Select(attrs={'class': 'form-select'}),
+            
+        }
+        def clean_date_time(self):
+            
+                date_time = self.cleaned_data.get('date_time')
+            
+            
+                if date_time and date_time < timezone.now():
+                
+                    raise ValidationError("Cannot schedule meetings in the past")
+                
+                return date_time
--- healthcheck/views.py
+++ healthcheck/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, redirect
+from django.utils import timezone
+import datetime
+from healthcheck.models import Meeting
 from .forms import MeetingForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
+from .models import Meeting
 
 
+# Authentication views
 def login_view(request):
     if request.method == "POST":
         user = authenticate(
@@ -35,6 +41,7 @@ def logout_view(request):
 
 
 
+# Views for core pages
 
 def dashboard_view(request):
     return render(request, "healthcheck/dashboard.html")
@@ -56,6 +63,7 @@ def message_view(request):
 def report_view(request):
     return render(request, "healthcheck/report.html")
 
+
 def schedule_view(request):
     if request.method == "POST":
         form = MeetingForm(request.POST)
@@ -65,14 +73,74 @@ def schedule_view(request):
     else:
         form = MeetingForm()
 
-    context = {"form": form}
+   
+    filter_type = request.GET.get('view', 'all')
+    now = timezone.now()
+    today_start = now.replace(hour=0, minute=0, second=0)
+    
+    if filter_type == 'today':
+        end_of_day = today_start + datetime.timedelta(days=1)
+        meetings = Meeting.objects.filter(date_time__range=[today_start, end_of_day]).order_by('date_time')
+    elif filter_type == 'weekly':
+        next_week = today_start + datetime.timedelta(days=7)
+        meetings = Meeting.objects.filter(date_time__range=[today_start, next_week]).order_by('date_time')
+    elif filter_type == 'monthly':
+        next_month = today_start + datetime.timedelta(days=30)
+        meetings = Meeting.objects.filter(date_time__range=[today_start, next_month]).order_by('date_time')
+    else:
+        meetings = Meeting.objects.all().order_by('date_time')
+
+   
+    calendar_days = []
+    for i in range(7):
+        current_day = today_start + datetime.timedelta(days=i)
+        next_day = current_day + datetime.timedelta(days=1)
+        
+        
+        day_meetings = Meeting.objects.filter(date_time__range=[current_day, next_day]).order_by('date_time')
+        
+        calendar_days.append({
+            'date_string': current_day.strftime('%a'), # "Mon", "Tue"
+            'day_number': current_day.strftime('%d/%m'), # "09/04"
+            'meetings': day_meetings
+        })
+
+    context = {
+        "form": form, 
+        "meetings": meetings, 
+        "current_filter": filter_type,
+        "calendar_days": calendar_days # Pass the calendar data to HTML
+    }
     return render(request, "healthcheck/schedule.html", context)
+# Function to edit an existing meeting
+def edit_meeting(request, meeting_id):
+    meeting = get_object_or_404(Meeting, pk=meeting_id)
+    
+    if request.method == "POST":
+        form = MeetingForm(request.POST, instance=meeting)
+        if form.is_valid():
+            form.save()
+            return redirect('schedule')
+    else:
+        
+        form = MeetingForm(instance=meeting)
+    
+    
+    return render(request, 'healthcheck/schedule.html', {'form': form, 'editing': True})
+
+
+# Function to delete a meeting
+def delete_meeting(request, meeting_id):
+    meet = get_object_or_404(Meeting, pk=meeting_id)
+    meet.delete()
+    return redirect('schedule')
+
 
 def profile_view(request):
     return render(request, "healthcheck/profile.html")
 
 
-## none critical pages below
+## view for none critical pages below
 
 def help_view(request):
     return render(request, 'healthcheck/help.html')
