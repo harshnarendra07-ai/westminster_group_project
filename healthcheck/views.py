2d70512fb330dbf5ba23a6804d1c5c1b148afcbb
subhan imran <w2045834@westminster.ac.uk>
2026-04-12 23:37:47 +0100

    Saved views.py fixes before pulling

--- healthcheck/views.py
+++ healthcheck/views.py
@@ -2,11 +2,10 @@ from django.shortcuts import render, redirect
 from django.shortcuts import get_object_or_404, redirect
 from django.utils import timezone
 import datetime
-from healthcheck.models import Meeting
 from .forms import MeetingForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
-from .models import Meeting
+from .models import Meeting, Department, Team
 
 
 # Authentication views
@@ -23,6 +22,7 @@ def login_view(request):
 
     return render(request, "healthcheck/login.html")
 
+
 def signup_view(request):
     if request.method == "POST":
         user = User.objects.create_user(
@@ -34,30 +34,56 @@ def signup_view(request):
 
     return render(request, "healthcheck/signup.html")
 
+
 def logout_view(request):
     logout(request)
     return redirect("login")
 
 
-
-
 # Views for core pages
-
 def dashboard_view(request):
-    return render(request, "healthcheck/dashboard.html")
+    # get all departments and teams
+    departments = Department.objects.all()
+    teams = Team.objects.all()
+
+    labels = []
+    values = []
+
+    # count teams in each department
+    for dept in departments:
+        labels.append(dept.dept_name)
+        team_count = Team.objects.filter(department=dept).count()
+        values.append(team_count)
+
+    # get next 3 meetings for dashboard
+    meetings = Meeting.objects.order_by('date_time')[:3]
+
+    # send data to dashboard page
+    context = {
+        "labels": labels,
+        "values": values,
+        "total_departments": departments.count(),
+        "total_teams": teams.count(),
+        "meetings": meetings,
+    }
+
+    return render(request, "healthcheck/dashboard.html", context)
+
 
 def teams_view(request):
     return render(request, "healthcheck/team.html")
 
+
 def department_view(request):
     return render(request, "healthcheck/department.html")
+
 
 def organisation_view(request):
     return render(request, "healthcheck/organisation.html")
 
+
 def message_view(request):
     return render(request, "healthcheck/message.html")
-
 
 
 def report_view(request):
@@ -73,11 +99,10 @@ def schedule_view(request):
     else:
         form = MeetingForm()
 
-   
     filter_type = request.GET.get('view', 'all')
     now = timezone.now()
     today_start = now.replace(hour=0, minute=0, second=0)
-    
+
     if filter_type == 'today':
         end_of_day = today_start + datetime.timedelta(days=1)
         meetings = Meeting.objects.filter(date_time__range=[today_start, end_of_day]).order_by('date_time')
@@ -90,42 +115,41 @@ def schedule_view(request):
     else:
         meetings = Meeting.objects.all().order_by('date_time')
 
-   
     calendar_days = []
     for i in range(7):
         current_day = today_start + datetime.timedelta(days=i)
         next_day = current_day + datetime.timedelta(days=1)
-        
-        
+
         day_meetings = Meeting.objects.filter(date_time__range=[current_day, next_day]).order_by('date_time')
-        
+
         calendar_days.append({
-            'date_string': current_day.strftime('%a'), # "Mon", "Tue"
-            'day_number': current_day.strftime('%d/%m'), # "09/04"
+            'date_string': current_day.strftime('%a'),
+            'day_number': current_day.strftime('%d/%m'),
             'meetings': day_meetings
         })
 
     context = {
-        "form": form, 
-        "meetings": meetings, 
+        "form": form,
+        "meetings": meetings,
         "current_filter": filter_type,
-        "calendar_days": calendar_days # Pass the calendar data to HTML
+        "calendar_days": calendar_days
     }
+
     return render(request, "healthcheck/schedule.html", context)
+
+
 # Function to edit an existing meeting
 def edit_meeting(request, meeting_id):
     meeting = get_object_or_404(Meeting, pk=meeting_id)
-    
+
     if request.method == "POST":
         form = MeetingForm(request.POST, instance=meeting)
         if form.is_valid():
             form.save()
             return redirect('schedule')
     else:
-        
         form = MeetingForm(instance=meeting)
-    
-    
+
     return render(request, 'healthcheck/schedule.html', {'form': form, 'editing': True})
 
 
@@ -140,10 +164,10 @@ def profile_view(request):
     return render(request, "healthcheck/profile.html")
 
 
-## view for none critical pages below
-
+# view for non critical pages
 def help_view(request):
     return render(request, 'healthcheck/help.html')
+
 
 def support_view(request):
     return render(request, "healthcheck/support.html")
