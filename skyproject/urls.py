c010d9e7f54f7349bc54d27464d11328d9096565
harshnarendra07 <harshnarendra07@gmail.com>
2026-04-18 11:42:27 +0100

    Security Fix: Fixed vulnerability in meeting views

--- skyproject/urls.py
+++ skyproject/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import RedirectView  
 from healthcheck import views
 from healthcheck.report_views import report_view
 
@@ -12,18 +13,22 @@ urlpatterns = [
     path('logout/', views.logout_view, name='logout'),
     path('signup/', views.signup_view, name='signup'),
 
+    
+    path('', RedirectView.as_view(pattern_name='login', permanent=False), name='root'),
+
     # Core Pages (Team's work)
-    path('', views.dashboard_view, name='dashboard'),
+    path('dashboard/', views.dashboard_view, name='dashboard'), 
     path('team/', views.team_view, name='team'),
     path('departments/', views.department_view, name='department'),
     path('organisation/', views.organisation_view, name='organisation'),
     path('messages/', views.message_view, name='message'),
     path('report/', report_view, name='report'),
     path('profile/', views.profile_view, name='profile'),
-    path('help/', views.help_view, name='help'),
-    path('support/', views.support_view, name='support'),
-    # Schedule Pages
     path('schedule/', views.schedule_view, name='schedule'),
     path('schedule/edit/<int:meeting_id>/', views.edit_meeting, name='edit_meeting'),
     path('schedule/delete/<int:meeting_id>/', views.delete_meeting, name='delete_meeting'),
+    path('help/', views.help_view, name='help'),
+    path('support/', views.support_view, name='support'),
+    
+    
 ]
